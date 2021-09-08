59cc16ab1441666e58b1575472d95f0749610089
Ui Okuyama <uiokuyama1997@gmail.com>
2021-09-09 02:33:01 +0900

    Create levelupViewController.

---
+++ MusicTermApp/Controllers/LevelUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LevelUpViewController.swift
+//  MusicTermApp
+//
+//  Created by 槙和馬 on 2021/09/09.
+//
+
+import Foundation
+import UIKit
+
+class LevelUpViewController: UIViewController {
+    
+    @IBOutlet weak var levelUpImageView: UIImageView!
+    @IBAction func tappedCloseButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
